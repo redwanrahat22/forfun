1f2be226b1daa1bb7762da5a3dcb8c389a90df7d
Dark <redwan.rahat69@gmail.com>
2023-12-25 22:45:14 +0600

    first commit

---
+++ DOM.js
@@ -0,0 +1,27 @@
+
+const ans = document.getElementById("dontYes");
+const call = document.getElementById("Note").innerText;
+function checker1(){
+    
+
+    
+    document.getElementById("one-div").style.display='block';
+    document.getElementById("notshow").style.display="none";
+
+    ans.innerText = call;
+}
+
+
+function checker2(){
+    const ans = document.getElementById("dontYes");
+
+    ans.innerText = "GAY SPOTED";
+
+}
+
+function checker3(){
+    ans.innerText = "Your 'THOBRA' does not support girls";
+}
+function checker4(){
+    ans.innerText = "Fuck OFF";
+}
